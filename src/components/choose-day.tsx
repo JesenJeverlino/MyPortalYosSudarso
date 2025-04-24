609c45a99b85ec0b07c5a7682863ea822427b592
Jesen Jeverlino <jesenjeverlino22@gmail.com>
2025-04-24 21:20:03 +0700

    classes and schedule done

---
+++ src/components/choose-day.tsx
@@ -0,0 +1,13 @@
+export default function ChooseDay(){
+    return(
+        <>
+      <div className="mt-20 w-[90%] flex justify-between">
+        <div className="bg-[#5398ff] w-[18%] h-[70px] rounded-[30px] flex items-center justify-center text-white text-2xl font-bold">MONDAY</div>
+        <div className="bg-[#5398ff] w-[18%] h-[70px] rounded-[30px] flex items-center justify-center text-white text-2xl font-bold">MONDAY</div>
+        <div className="bg-[#5398ff] w-[18%] h-[70px] rounded-[30px] flex items-center justify-center text-white text-2xl font-bold">MONDAY</div>
+        <div className="bg-[#5398ff] w-[18%] h-[70px] rounded-[30px] flex items-center justify-center text-white text-2xl font-bold">MONDAY</div>
+        <div className="bg-[#5398ff] w-[18%] h-[70px] rounded-[30px] flex items-center justify-center text-white text-2xl font-bold">MONDAY</div>
+      </div>
+        </>
+    )
+}
--- src/page/user-page/user-class-and-schedule.tsx
+++ src/page/user-page/user-class-and-schedule.tsx
@@ -1,5 +1,7 @@
 import Sidebar from "@/components/sidebar";
 import Header from "@/components/header";
+import ChooseDay from "@/components/choose-day";
+import Table from "@/components/table";
 
 export default function UserClassAndSchedule() {
   return (
@@ -10,6 +12,8 @@ export default function UserClassAndSchedule() {
         <main className="flex-1 min-h-screen bg-[#1952a6] overflow-auto">
           <div className="flex flex-col items-center w-full h-full bg-white rounded-l-2xl p-5">
             <Header></Header>
+            <ChooseDay></ChooseDay>
+            <Table></Table>
           </div>
         </main>
       </div>
