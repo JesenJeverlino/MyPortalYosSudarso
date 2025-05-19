5175dce863d7a300aca8942ab0422fbb4b841d10
Jesen Jeverlino <jesenjeverlino22@gmail.com>
2025-05-19 07:02:36 +0700

    solve problem image files gabisa akses wwwroot

--- src/components/other-component/student-card.tsx
+++ src/components/other-component/student-card.tsx
@@ -16,11 +16,15 @@ export default function StudentCard({
   userId, //simpan aja
   navigateTo,
 }: StudentCardProps) {
+
+  console.log(imagePath);
   return (
     <>
       <div className="flex justify-between items-center p-2 border mb-5 md:flex-row flex-col md:gap-0 gap-5 w-[99%]">
         <img
-          src={imagePath || "default.jpg"} //ini harus kasi path backend 
+          src={
+            imagePath ? `https://localhost:44364/${imagePath}` : "/default.jpg"
+          }
           className="xl:w-[8%] md:w-[10%] sm:w-[15%] w-[25%] h-auto rounded-full xl:mr-0 mr-5"
         />
         <h1 className="flex-start md:w-[70%] w-[100%] md:text-left text-center xl:text-2xl md:text-xl text-lg truncate">
